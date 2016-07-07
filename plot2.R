df91410ba75a3320771371672641aab5a2d25f85
wdsteck <wdsteck@yahoo.com>
2016-07-07 12:12:51 -0400

    Created new R scripts for assignment 1. Also included the resulting chart files.

--- plot2.R
+++ plot2.R
@@ -1,19 +1,20 @@
 #
-# plot1.R script
+# plot2.R script
 #
 # This script reads electrical usage data of a sample home and creates
 # some plots from that data usimg the base plotting system.
 #
 # The data is located at: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 #
-# This script creates the first plot. It is found in the class
+# This script creates the second plot. It is found in the class
 # repo here: https://github.com/rdpeng/ExData_Plotting1
 #
-# The plot is a histogram of the frequency of the Global Active Power reading
-# over a 2 day period (2007-02-01 and 2007-02-02).
+# The plot is a XY line plot of the Global Active Power usage
+# over time (2007-02-01 and 2007-02-02).
 # 
 # This script reads the raw data, extracts the information for the dates
-# requested and creates the plot.
+# requested and creates the plot. A new column is created holding the time/date
+# structure (combination of the Time column and the Date column)
 #
 
 # Download the data file and extract it from the zip if the data file is not local
@@ -21,7 +22,7 @@
 dataFileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 dataFileZip <- "data.zip"
 dataFileSrc <- "household_power_consumption.txt"
-dataFileOut <- "plot1.png"
+dataFileOut <- "plot2.png"
 
 if (!file.exists(dataFileSrc)) {
         if (!file.exists(dataFileZip)) {
@@ -51,18 +52,20 @@ df$Date <- as.Date(df$Date, "%d/%m/%Y")
 # extract only the dates we care about
 df <- with(df, df[(Date >= "2007-02-01") & (Date <= "2007-02-02"), ])
 
+# add new col for time/date field
+df$TimeDate <- as.POSIXct(strptime(paste(df$Date, df$Time), "%Y-%m-%d %H:%M:%S"))
+
 # Create the chart
 
 # first open the png device.
 png(dataFileOut, width = 480, height = 480)
 
-# create the histogram
+# create the xy line plot
 
-hist(df$Global_active_power,
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)",
-     ylab = "Frequency",
-     col = "red"
+plot(df$TimeDate, df$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)"
      )
 
 # Close teh device
--- plot4.R
+++ plot4.R
@@ -1,16 +1,20 @@
 #
-# plot1.R script
+# plot4.R script
 #
 # This script reads electrical usage data of a sample home and creates
-# some plots from that data usimg the base plotting system.
+# some plots from that data using the base plotting system.
 #
 # The data is located at: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 #
-# This script creates the first plot. It is found in the class
+# This script creates the fourth plot. It is found in the class
 # repo here: https://github.com/rdpeng/ExData_Plotting1
 #
-# The plot is a histogram of the frequency of the Global Active Power reading
+# The plot has 4 graphs on it (2x2). Each data platted as a function of time
 # over a 2 day period (2007-02-01 and 2007-02-02).
+# - The first plots the Global Active Power (same as what plot2.R produces)
+# - The next plots the voltage.
+# - The next plots the sub metering power (same as what plot3.R produces)
+# - The last plots the global reactive power.
 # 
 # This script reads the raw data, extracts the information for the dates
 # requested and creates the plot.
@@ -21,7 +25,7 @@
 dataFileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 dataFileZip <- "data.zip"
 dataFileSrc <- "household_power_consumption.txt"
-dataFileOut <- "plot1.png"
+dataFileOut <- "plot4.png"
 
 if (!file.exists(dataFileSrc)) {
         if (!file.exists(dataFileZip)) {
@@ -51,19 +55,56 @@ df$Date <- as.Date(df$Date, "%d/%m/%Y")
 # extract only the dates we care about
 df <- with(df, df[(Date >= "2007-02-01") & (Date <= "2007-02-02"), ])
 
+# add new col for time/date field
+df$TimeDate <- as.POSIXct(strptime(paste(df$Date, df$Time), "%Y-%m-%d %H:%M:%S"))
+
 # Create the chart
 
 # first open the png device.
 png(dataFileOut, width = 480, height = 480)
 
-# create the histogram
+# Set up the plot area
+par(mfrow=c(2,2))
 
-hist(df$Global_active_power,
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)",
-     ylab = "Frequency",
-     col = "red"
+# Plot the 1st chart
+
+plot(df$TimeDate, df$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)"
+)
+
+# Plot the 2nd chart
+
+plot(df$TimeDate, df$Voltage,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage"
+)
+
+# Plot the 3rd chart
+plot(range(df$TimeDate), range(df$Sub_metering_1, df$Sub_metering_2, df$Sub_metering_3),
+     type = "n",
+     xlab = "",
+     ylab = "Energy Sub Metering"
      )
+lines(df$TimeDate,df$Sub_metering_1, type = "l", col = "black")
+lines(df$TimeDate,df$Sub_metering_2, type = "l", col = "red")
+lines(df$TimeDate,df$Sub_metering_3, type = "l", col = "blue")
+legend("topright",
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"),
+       cex = .8,
+       lty = 1
+       )
 
-# Close teh device
+# Plot the 4th chart
+
+plot(df$TimeDate, df$Global_reactive_power,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Global Reactive Power"
+)
+
+# Close the device
 dev.off()
